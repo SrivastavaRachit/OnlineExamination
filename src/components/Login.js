909f5b932af216f52d18104296d61665939304d4
m-triple-m <triplem656@gmail.com>
2021-09-13 18:29:59 +0530

    ..

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,6 +12,7 @@ const myStyles = makeStyles(() => ({
 		boxShadow: '0px 6px 6px -3px rgba(0,0,0,0.2),0px 10px 14px 1px rgba(0,0,0,0.14),0px 4px 18px 3px rgba(0,0,0,0.12)'
 	}
 }))
+
 const Login = () => {
 
 	const url = app_config.api_url;
